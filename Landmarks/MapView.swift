7dfd9664ae332e0be3c8ec7451e998590cb7b0df
sho <shohitomi25@gmail.com>
2023-09-23 20:51:54 +0900

    created MapView

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by sho on 2023/09/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var cameraPosition: MapCameraPosition {
+        MapCameraPosition.region(region)
+    }
+    
+    var body: some View {
+        Map(position: .constant(cameraPosition), bounds: nil, interactionModes: .all, scope: nil)
+    }
+}
+
+#Preview {
+    MapView()
+}
