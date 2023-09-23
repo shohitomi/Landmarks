cd7fc97bfc4a151b8390e3f5923a558b5e8c674f
sho <shohitomi25@gmail.com>
2023-09-23 19:24:59 +0900

    added text

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack {
+            Text("Turtle Rock")
+                .font(.title)
+            Text("Joshua Tree National Park")
+        }
     }
 }
 
