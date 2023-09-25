f85b0648db799ec6e4d348c2dcb7abb1929d6c03
sho <shohitomi25@gmail.com>
2023-09-25 12:05:46 +0900

    created row

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by sho on 2023/09/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }.previewLayout(.fixed(width: 300, height: 70))
+}
